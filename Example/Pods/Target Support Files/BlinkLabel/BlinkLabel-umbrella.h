9c8c59e42601b46e562bdf297d3e1a9f84a9bab7
Carlos Alberto Savi <carlos.savi@itau-unibanco.com.br>
2016-10-14 11:31:46 -0300

    Initial Commit

---
+++ Example/Pods/Target Support Files/BlinkLabel/BlinkLabel-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double BlinkLabelVersionNumber;
+FOUNDATION_EXPORT const unsigned char BlinkLabelVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-BlinkLabel_Tests/Pods-BlinkLabel_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BlinkLabel_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BlinkLabel_TestsVersionString[];
+
